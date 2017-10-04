b3f0ab4c86fa615fe5d73c0dd4578a56a0e1f564
Christian Feneberg <christian@fenepedia.de>
2017-10-04 16:27:31 +0200

    Contao 4 - Eintrag TopMenü
    Eintrag Videohandbuch in TopMenü hinzufügen

--- classes/AcademyHelper.php
+++ classes/AcademyHelper.php
@@ -17,12 +17,28 @@ class AcademyHelper extends System
 
 	public function HeaderVideohandbuch($strBuffer, $strTemplate)
 	{
-		if ($strTemplate == 'be_main')
-		{
-			$strSearch = '<span class="header_user_container">';
-			$strBefore = '<span class="header_academy_container"><a class="header_videohandbuch" href="contao/main.php?do=Videohandbuch" title="">Videohandbuch</a></span>';
-			$strBuffer = str_replace($strSearch,$strBefore.$strSearch,$strBuffer);
-		}
+
+        //Check Contao 4
+        if (version_compare(VERSION, '4.4', '<'))
+        {
+            // Code für Contao 3.5
+            if ($strTemplate == 'be_main')
+            {
+                $strSearch = '<span class="header_user_container">';
+                $strBefore = '<span class="header_academy_container"><a class="header_videohandbuch" href="contao/main.php?do=Videohandbuch" title="">Videohandbuch</a></span>';
+                $strBuffer = str_replace($strSearch,$strBefore.$strSearch,$strBuffer);
+            }
+        }
+        else
+        {
+            if ($strTemplate == 'be_main')
+            {
+                $strSearch = '<ul id="tmenu">';
+                $strAfter = '<li class="header_academy_container"><a class="tmenu_videohandbuch" href="contao?do=Videohandbuch" title="">Videohandbuch</a></li>';
+                $strBuffer = str_replace($strSearch,$strSearch.$strAfter,$strBuffer);
+            }
+        }
+
 
 		return $strBuffer;
 	}
