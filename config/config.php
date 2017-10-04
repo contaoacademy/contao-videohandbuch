c33241f2eebccf7fb60a1e66ae7c918e45946245
Christian Feneberg <christian@fenepedia.de>
2017-10-04 16:29:19 +0200

    Version korrigieren
    Version korrigieren

--- config/config.php
+++ config/config.php
@@ -7,7 +7,7 @@
  *
  */
 
-define('academy_client', '1.2');
+define('academy_client', '1.1.2');
 
 if(file_exists(TL_ROOT . '/system/modules/contao_academy_client/classes/AcademyHelper.php')):
 $GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = array('AcademyHelper', 'HeaderVideohandbuch');
